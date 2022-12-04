8d3ccfa63114807819849b763b13e757d9595d2c
prateek12399 <109479422+prateek12399@users.noreply.github.com>
2022-12-04 19:29:23 +0530

    Add files via upload

---
+++ assigment prm2.py
@@ -0,0 +1,10 @@
+CREATE DATABASE ASSIGNMENT;
+USE ASSIGNMENT;
+
+
+CREATE TABLE countries(
+COUNTRY_ID varchar(2),
+COUNTRY_NAME varchar(40)
+CHECK(COUNTR_NAME IN('Italy','India','China'))
+REGION_ID decimal(10,0)
+);
